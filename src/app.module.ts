4c63c2d2e7fcfb413f1d96c4280a055db6e0aba0
Ludovic_Domingues <ludovic.domingues96@gmail.com>
2025-03-09 09:53:19 +0100

    started work on user

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { JobModule } from './job/job.module';
 import { SkillModule } from './skill/skill.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [JobModule, SkillModule],
+  imports: [JobModule, SkillModule, UsersModule],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/users/entities/user.entity.ts
@@ -0,0 +1,9 @@
+export class User {
+    id: string;          // Unique identifier (consider using UUID)
+    email: string;
+    password: string;    // Store hashed password only
+    name: string;
+    createdAt: Date;
+    updatedAt: Date;
+}
+  
